ea289c4bceb27b48b86ee1e24f3beb77a84bd488
jaredtao <jared2020@163.com>
2020-01-12 02:54:34 +0800

    update path for ubuntu

--- syncSkia-gitee.sh
+++ syncSkia-gitee.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -e
 if [ ! -d "3rdparty/skia" ]; then
     git clone "https://gitee.com/QtSkia/skia.git" --branch QtSkia --depth 1 3rdparty/skia
-    pushd 3rdparty/skia
+    cd 3rdparty/skia
 else
-    pushd 3rdparty/skia
+    cd 3rdparty/skia
     git pull --depth=1 "https://gitee.com/QtSkia/skia.git" QtSkia --allow-unrelated-histories
 fi
 export GIT_SYNC_DEPS_PATH=$PWD/DEPS-gitee
 python tools/git-sync-deps -v
-popd
+cd ../..
--- syncSkia.sh
+++ syncSkia.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -e
 if [ ! -d "3rdparty/skia" ]; then
     git clone "https://github.com/QtSkia/skia.git" --branch QtSkia --depth 1 3rdparty/skia
-    pushd 3rdparty/skia
+    cd 3rdparty/skia
 else
-    pushd 3rdparty/skia
+    cd 3rdparty/skia
     git pull --depth=1 "https://github.com/QtSkia/skia.git" QtSkia --allow-unrelated-histories
 fi
 export GIT_SYNC_DEPS_PATH=$PWD/DEPS-github
 python tools/git-sync-deps -v
-popd
+cd ../..
