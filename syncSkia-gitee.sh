6e8fdcaa23939b86790d786d2c70abdf614200b5
jaredtao <jaredtao@jaredtaos-iMac.local>
2020-01-11 05:02:33 -0800

    update for macos

--- syncSkia-gitee.sh
+++ syncSkia-gitee.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -e
-git clone "https://gitee.com/QtSkia/skia.git" --no-checkout --depth 1 3rdparty/skia
+if [ ! -d "3rdparty/skia" ]; then
+    git clone "https://gitee.com/QtSkia/skia.git" --no-checkout --depth 1 3rdparty/skia
+fi
 pushd 3rdparty/skia
 git pull --depth=1 "https://gitee.com/QtSkia/skia"
 export GIT_SYNC_DEPS_PATH=$PWD/DEPS-gitee
