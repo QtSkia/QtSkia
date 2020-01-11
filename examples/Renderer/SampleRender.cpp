be314fcc18a87fe0ae39689f21a19b90bedcb568
jaredtao <jared2020@163.com>
2020-01-12 03:30:16 +0800

    good night

--- examples/Renderer/SampleRender.cpp
+++ examples/Renderer/SampleRender.cpp
@@ -1,19 +1,20 @@
 #include "SampleRender.h"
-#include "core/SkFont.h"
-#include "core/SkCanvas.h"
-#include "core/SkPaint.h"
-void SampleRender::draw(SkCanvas *canvas, int elapsed, int w, int h) 
+
+SampleRender::SampleRender()
+{
+    m_paint.setAntiAlias(true);
+    m_paint.setColor(SK_ColorRED);
+    m_paint.setStrokeWidth(2.0f);
+    m_font.setSize(30);
+}
+
+void SampleRender::draw(SkCanvas *canvas, int elapsed, int w, int h)
 {
     m_rotateAngle = int(elapsed * m_rotateSpeed + m_rotateAngle) % 360;
-    SkPaint p;
-    p.setAntiAlias(true);
-    p.setColor(SK_ColorRED);
-    p.setStrokeWidth(2.0f);
+
     canvas->clear(SK_ColorWHITE);
-    SkFont font;
-    font.setSize(30);
     canvas->rotate(m_rotateAngle, w / 2, h /2);
-    canvas->drawString("Hello Skia", w / 2 - 20, h  /2, font, p);
-    canvas->drawLine(w * 0.2f, h * 0.2f, w * 0.4f, h * 0.4f, p);
+    canvas->drawString("Hello Skia", w / 2 - 20, h  /2, m_font, m_paint);
+    canvas->drawLine(w * 0.2f, h * 0.2f, w * 0.4f, h * 0.4f, m_paint);
     canvas->flush();
 }
--- examples/Renderer/SampleRender.h
+++ examples/Renderer/SampleRender.h
@@ -1,11 +1,16 @@
 #pragma once
 #include "IRender.h"
-class SkCanvas;
+#include "core/SkFont.h"
+#include "core/SkPaint.h"
+#include "core/SkCanvas.h"
 
 class SampleRender : public IRender{
 public:
+    SampleRender();
     void draw(SkCanvas *canvas, int elapsed, int w, int h) override;
 private:
+    SkPaint m_paint;
+    SkFont m_font;
     float m_rotateSpeed = 90.0f / 1000;
     int m_rotateAngle = 0;
 };
