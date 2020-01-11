3e1df03eee181ab75c6d7ea32443c675581ea2a7
jaredtao <jared2020@163.com>
2020-01-12 01:28:49 +0800

    allow-unrelated-histories

--- syncSkia.sh
+++ syncSkia.sh
@@ -3,7 +3,7 @@ if [ ! -d "3rdparty/skia" ]; then
     git clone "https://github.com/QtSkia/skia.git" --no-checkout --depth 1 3rdparty/skia
 fi
 pushd 3rdparty/skia
-git pull --depth=1 "https://github.com/QtSkia/skia"
+git pull --depth=1 "https://github.com/QtSkia/skia" --allow-unrelated-histories
 export GIT_SYNC_DEPS_PATH=$PWD/DEPS-github
 python tools/git-sync-deps -v
 popd
